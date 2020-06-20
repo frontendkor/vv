a7007fcd7122b5ebb9d81ff19730734b40344972
frontendkor <frontendkor@gmail.com>
2020-06-20 13:19:25 +0300

    Updates

---
+++ precache-manifest.6e5735cf18f7d912010cab61a71ed3cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4031dcc1f8db89e6d635c0d65559c27e",
+    "url": "/vv/index.html"
+  },
+  {
+    "revision": "616d879a27232a148293",
+    "url": "/vv/static/css/main.2cf9d3de.chunk.css"
+  },
+  {
+    "revision": "79d4c6127bdf6a2750d8",
+    "url": "/vv/static/js/2.c8855a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vv/static/js/2.c8855a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616d879a27232a148293",
+    "url": "/vv/static/js/main.ff3120ca.chunk.js"
+  },
+  {
+    "revision": "528726080369f2351a45",
+    "url": "/vv/static/js/runtime-main.a68d7692.js"
+  }
+]);
